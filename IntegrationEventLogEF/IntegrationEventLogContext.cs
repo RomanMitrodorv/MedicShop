8092e781c685444663ce8cf55e4da8920ed08dde
Roman <mitrodorov@gmail.com>
2022-01-07 23:05:06 +0300

    add event log ef

---
+++ IntegrationEventLogEF/IntegrationEventLogContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationEventLogEF
+{
+    public class IntegrationEventLogContext : DbContext
+    {
+        public IntegrationEventLogContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<IntegrationEventLogEntry> IntegrationEventLogs { get;set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) => modelBuilder.Entity<IntegrationEventLogEntry>(ConfigureIntegrationEventLogEntry);
+
+        private void ConfigureIntegrationEventLogEntry(EntityTypeBuilder<IntegrationEventLogEntry> builder)
+        {
+            builder.ToTable("IntegrationEventLog");
+
+            builder.HasKey(e => e.EventId);
+
+            builder.Property(e => e.EventId)
+                .IsRequired();
+
+            builder.Property(e => e.Content)
+                .IsRequired();
+
+            builder.Property(e => e.CreationTime)
+                .IsRequired();
+
+            builder.Property(e => e.State)
+                .IsRequired();
+
+            builder.Property(e => e.EventTypeName)
+                .IsRequired();
+
+            builder.Property(e => e.TimesSent)
+                .IsRequired();
+
+        }
+
+    }
+}
---
+++ IntegrationEventLogEF/Services/IntegrationEventLogService.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationEventLogEF.Services
+{
+    public class IntegrationEventLogService : IIntegrationEventLogService, IDisposable
+    {
+        private readonly IntegrationEventLogContext _context;
+        private readonly DbConnection _dbConnection;
+        private readonly List<Type> _eventTypes;
+        private volatile bool _disposed;
+
+
+        public IntegrationEventLogService(DbConnection dbConnection)
+        {
+            _dbConnection = dbConnection ?? throw new ArgumentNullException(nameof(dbConnection));
+
+            _context = new IntegrationEventLogContext(
+                new DbContextOptionsBuilder<IntegrationEventLogContext>()
+                    .UseSqlServer(_dbConnection)
+                    .Options);
+
+            _eventTypes = Assembly.Load(Assembly.GetEntryAssembly().FullName)
+                .GetTypes()
+                .Where(x => x.Name.EndsWith(nameof(IntegrationEvent)))
+                .ToList();
+
+        }
+
+
+        private Task UpdateEventStatus(Guid eventId, EventStateEnum state)
+        {
+            var @event = _context.IntegrationEventLogs.Single(x => x.EventId == eventId);
+            @event.State = state;
+
+            if (@event.State == EventStateEnum.InProgress)
+                @event.TimesSent += 1;
+
+            _context.IntegrationEventLogs.Update(@event);
+
+            return _context.SaveChangesAsync();
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if(!_disposed)
+            {
+                if (disposing)
+                {
+                    _context?.Dispose();
+                }
+
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public async Task<IEnumerable<IntegrationEventLogEntry>> GetEventLogsPendingToPublishAsync(Guid transactionId)
+        {
+            var tid = transactionId.ToString();
+
+            var result = await _context.IntegrationEventLogs
+                .Where(x => x.TransactionId == tid && x.State == EventStateEnum.NotPublished)
+                .ToListAsync();
+
+            if (result != null && result.Any())
+            {
+                return result.OrderBy(o => o.CreationTime)
+                    .Select(e => e.DeserializeJsonContent(_eventTypes.Find(t => t.Name == e.EventTypeShortName)));
+            }
+
+            return new List<IntegrationEventLogEntry>();
+        }
+
+        public Task MarkEventAsFailedAsync(Guid eventId)
+        {
+            return UpdateEventStatus(eventId, EventStateEnum.PublishedFailed);
+        }
+
+        public Task MarkEventAsInProgressAsync(Guid eventId)
+        {
+            return UpdateEventStatus(eventId, EventStateEnum.InProgress);
+        }
+
+        public Task MarkEventAsPublishedAsync(Guid eventId)
+        {
+            return UpdateEventStatus(eventId, EventStateEnum.Published);
+        }
+
+        public Task SaveEventAsync(IntegrationEvent @event, IDbContextTransaction transaction)
+        {
+            if (transaction == null)
+                throw new ArgumentNullException(nameof(transaction));
+
+            var eventLogEntry = new IntegrationEventLogEntry(@event, transaction.TransactionId);
+
+            _context.Database.UseTransaction(transaction.GetDbTransaction());
+            _context.IntegrationEventLogs.Add(eventLogEntry);
+
+            return _context.SaveChangesAsync();
+        }
+    }
+}
